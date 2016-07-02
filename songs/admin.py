0949ad7c7d92c53599a76621568974b0d73be764
Martin Swift <martin@swift.is>
2016-07-02 14:36:14 +0000

    Added a new column and some filtering to the Song admin.

--- songs/admin.py
+++ songs/admin.py
@@ -8,6 +8,8 @@ class SongPhraseInline(admin.TabularInline):
 
 class SongAdmin(admin.ModelAdmin):
 	inlines = (SongPhraseInline,)
+	list_display = ('__str__', 'area_and_season', 'singer')
+	list_filter = ('area_and_season', 'time_begin')
 
 admin.site.register(Phrase)
 admin.site.register(Song,SongAdmin)
